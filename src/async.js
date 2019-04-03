921b7fbae6af8361100dd91f9bc2ffc35a9fa1ed
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-03 11:11:08 +0800

    Create async.js

---
+++ src/async.js
@@ -0,0 +1,55 @@
+/* eslint-disable class-methods-use-this */
+import AbortController from 'abort-controller';
+import SchedulerInterface from './scheduler-interface';
+
+let INSTANCE;
+/**
+ * A Scheduler that allows async scheduling on the current thread.
+ */
+export default class AsyncScheduler extends SchedulerInterface {
+  static get instance() {
+    if (typeof INSTANCE === 'undefined') {
+      INSTANCE = new AsyncScheduler();
+    }
+    return INSTANCE;
+  }
+
+  /**
+   * Schedules the function immediately on the async task.
+   * @param {!function} fn
+   */
+  schedule(fn) {
+    if (typeof fn === 'function') {
+      // eslint-disable-next-line no-new
+      Promise.resolve().then(() => fn());
+    }
+  }
+
+  /**
+   * Schedules the given function at a delayed time on the async task.
+   * @param {!function} fn
+   * A function that is called after being scheduled.
+   * @param {!number} amount
+   * The amount of delay in milliseconds.
+   * @returns {AbortController}
+   * Returns an AbortController that allows
+   * to abort the schedule.
+   */
+  delay(fn, amount) {
+    const controller = new AbortController();
+    if (typeof fn === 'function') {
+      const { signal } = controller;
+
+      // eslint-disable-next-line no-new
+      Promise.resolve().then(() => {
+        if (signal.aborted) {
+          return;
+        }
+        const inner = setTimeout(fn, amount);
+
+        signal.addEventListener('abort', () => clearTimeout(inner));
+      });
+    }
+    return controller;
+  }
+}
