69aff6007a0c4698aff124017874fd05e2787ad6
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-03 09:57:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import CurrentScheduler from './current/index';
+/* eslint-disable class-methods-use-this */
+import ImmediateCurrentScheduler from './immediate';
+import MicroCurrentScheduler from './micro';
+import MacroCurrentScheduler from './macro';
 
 export default class Scheduler {
-  static get current() {
-    return CurrentScheduler;
+  get immediate() {
+    return ImmediateCurrentScheduler.instance;
+  }
+
+  get micro() {
+    return MicroCurrentScheduler.instance;
+  }
+
+  get macro() {
+    return MacroCurrentScheduler.instance;
   }
 }
