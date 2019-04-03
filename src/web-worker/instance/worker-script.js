8045a010a418d5b2614411104ca2082a4dd1ead2
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-03 09:51:10 +0800

    Update worker-script.js

--- src/web-worker/instance/worker-script.js
+++ src/web-worker/instance/worker-script.js
@@ -9,12 +9,31 @@ self.addEventListener('message', (e) => {
    */
   const { func, id } = data;
 
+  let result;
   try {
-    eval(`const FUNCTION_ID = ${id}; (${func})()`);
+    result = eval(`const FUNCTION_ID = ${id}; (${func})()`);
   } catch (err) {
     self.postMessage({
       status: 'error',
       value: err,
+    });
+  }
+
+  if (result instanceof Promise) {
+    result.then(
+      x => self.postMessage({
+        status: 'success',
+        value: x,
+      }),
+      err => self.postMessage({
+        status: 'error',
+        value: err,
+      }),
+    );
+  } else {
+    self.postMessage({
+      status: 'success',
+      value: result,
     });
   }
 });
