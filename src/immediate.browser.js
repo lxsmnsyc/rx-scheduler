374b14d24eb118f87b79ab5c6e7173ef4fe9e4bb
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-03 11:11:11 +0800

    Create immediate.browser.js

---
+++ src/immediate.browser.js
@@ -0,0 +1,54 @@
+/* eslint-disable class-methods-use-this */
+import AbortController from 'abort-controller';
+import SchedulerInterface from './scheduler-interface';
+
+let INSTANCE;
+
+/**
+ * A Scheduler that allows immediate scheduling, using requestAnimationFrame.
+ */
+export default class ImmediateScheduler extends SchedulerInterface {
+  static get instance() {
+    if (typeof INSTANCE === 'undefined') {
+      INSTANCE = new ImmediateScheduler();
+    }
+    return INSTANCE;
+  }
+
+  /**
+   * Schedules the function immediately.
+   * @param {!function} fn
+   */
+  schedule(fn) {
+    if (typeof fn === 'function') {
+      requestAnimationFrame(fn);
+    }
+  }
+
+  /**
+   * Schedules the given function at a delayed time.
+   * @param {!function} fn
+   * A function that is called after being scheduled.
+   * @param {!number} amount
+   * The amount of delay in milliseconds.
+   * @returns {AbortController}
+   * Returns an AbortController that allows
+   * to abort the schedule.
+   */
+  delay(fn, amount) {
+    const controller = new AbortController();
+    if (typeof fn === 'function') {
+      const { signal } = controller;
+      const handler = requestAnimationFrame(() => {
+        const inner = setTimeout(() => {
+          fn();
+          controller.abort();
+        }, amount);
+
+        signal.addEventListener('abort', () => clearTimeout(inner));
+      });
+      signal.addEventListener('abort', () => cancelAnimationFrame(handler));
+    }
+    return controller;
+  }
+}
