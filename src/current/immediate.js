1a7a226d7b5d3c646847da68d5e5b1dbccbafa51
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-03 08:55:45 +0800

    Update immediate.js

--- src/current/immediate.js
+++ src/current/immediate.js
@@ -1,14 +1,41 @@
+/* eslint-disable class-methods-use-this */
 import AbortController from 'abort-controller';
+import SchedulerInterface from '../scheduler-interface';
 
-export default class ImmediateCurrentScheduler {
-  static schedule(fn) {
-    if (typeof fn === 'function') {
-      return fn();
+let INSTANCE;
+
+/**
+ * A Scheduler that allows immediate scheduling on the current thread.
+ */
+export default class ImmediateCurrentScheduler extends SchedulerInterface {
+  static get instance() {
+    if (typeof INSTANCE === 'undefined') {
+      INSTANCE = new ImmediateCurrentScheduler();
     }
-    return undefined;
+    return INSTANCE;
   }
 
-  static delay(fn, amount) {
+  /**
+   * Schedules the function immediately on the current task.
+   * @param {!function} fn
+   */
+  schedule(fn) {
+    if (typeof fn === 'function') {
+      fn();
+    }
+  }
+
+  /**
+   * Schedules the given function at a delayed time on the current task.
+   * @param {!function} fn
+   * A function that is called after being scheduled.
+   * @param {!number} amount
+   * The amount of delay in milliseconds.
+   * @returns {AbortController}
+   * Returns an AbortController that allows
+   * to abort the schedule.
+   */
+  delay(fn, amount) {
     const controller = new AbortController();
     if (typeof fn === 'function') {
       const { signal } = controller;
