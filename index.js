8fa613cd17d4f9eaa4b8c8323ac38e215bbd49f7
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-03 09:57:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,72 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 
 var AbortController = _interopDefault(require('abort-controller'));
 
-class ImmediateCurrentScheduler {
-  static schedule(fn) {
-    if (typeof fn === 'function') {
-      return fn();
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+/**
+ * @interface
+ */
+class SchedulerInterface {
+  /**
+   * Schedules the given function immediately.
+   * @param {!function} fn
+   * A function that is called after being scheduled.
+   *
+   * @abstract
+   */
+  schedule(fn) {}
+
+  /**
+   * Schedules the given function at a delayed time.
+   * @param {!function} fn
+   * A function that is called after being scheduled.
+   * @param {!number} amount
+   * The amount of delay in milliseconds.
+   * @returns {AbortController}
+   * Returns an AbortController that allows
+   * to abort the schedule.
+   * @abstract
+   */
+  delay(fn, amount) {
+  }
+}
+
+/* eslint-disable class-methods-use-this */
+
+let INSTANCE;
+
+/**
+ * A Scheduler that allows immediate scheduling on the current thread.
+ */
+class ImmediateScheduler extends SchedulerInterface {
+  static get instance() {
+    if (typeof INSTANCE === 'undefined') {
+      INSTANCE = new ImmediateScheduler();
     }
-    return undefined;
+    return INSTANCE;
   }
 
-  static delay(fn, amount) {
+  /**
+   * Schedules the function immediately on the current task.
+   * @param {!function} fn
+   */
+  schedule(fn) {
+    if (typeof fn === 'function') {
+      fn();
+    }
+  }
+
+  /**
+   * Schedules the given function at a delayed time on the current task.
+   * @param {!function} fn
+   * A function that is called after being scheduled.
+   * @param {!number} amount
+   * The amount of delay in milliseconds.
+   * @returns {AbortController}
+   * Returns an AbortController that allows
+   * to abort the schedule.
+   */
+  delay(fn, amount) {
     const controller = new AbortController();
     if (typeof fn === 'function') {
       const { signal } = controller;
@@ -25,15 +82,42 @@ class ImmediateCurrentScheduler {
   }
 }
 
-class MicroCurrentScheduler {
-  static schedule(fn) {
+/* eslint-disable class-methods-use-this */
+
+let INSTANCE$1;
+/**
+ * A Scheduler that allows micro scheduling on the current thread.
+ */
+class MicroScheduler extends SchedulerInterface {
+  static get instance() {
+    if (typeof INSTANCE$1 === 'undefined') {
+      INSTANCE$1 = new MicroScheduler();
+    }
+    return INSTANCE$1;
+  }
+
+  /**
+   * Schedules the function immediately on the micro task.
+   * @param {!function} fn
+   */
+  schedule(fn) {
     if (typeof fn === 'function') {
       // eslint-disable-next-line no-new
       Promise.resolve().then(() => fn());
     }
   }
 
-  static delay(fn, amount) {
+  /**
+   * Schedules the given function at a delayed time on the micro task.
+   * @param {!function} fn
+   * A function that is called after being scheduled.
+   * @param {!number} amount
+   * The amount of delay in milliseconds.
+   * @returns {AbortController}
+   * Returns an AbortController that allows
+   * to abort the schedule.
+   */
+  delay(fn, amount) {
     const controller = new AbortController();
     if (typeof fn === 'function') {
       const { signal } = controller;
@@ -52,15 +136,42 @@ class MicroCurrentScheduler {
   }
 }
 
-class MacroCurrentScheduler {
-  static schedule(fn) {
+/* eslint-disable class-methods-use-this */
+
+let INSTANCE$2;
+/**
+ * A Scheduler that allows macro scheduling on the current thread.
+ */
+class MacroScheduler extends SchedulerInterface {
+  static get instance() {
+    if (typeof INSTANCE$2 === 'undefined') {
+      INSTANCE$2 = new MacroScheduler();
+    }
+    return INSTANCE$2;
+  }
+
+  /**
+   * Schedules the function immediately on the macro task.
+   * @param {!function} fn
+   */
+  schedule(fn) {
     if (typeof fn === 'function') {
       // eslint-disable-next-line no-new
       setTimeout(fn, 0);
     }
   }
 
-  static delay(fn, amount) {
+  /**
+   * Schedules the given function at a delayed time on the macro task.
+   * @param {!function} fn
+   * A function that is called after being scheduled.
+   * @param {!number} amount
+   * The amount of delay in milliseconds.
+   * @returns {AbortController}
+   * Returns an AbortController that allows
+   * to abort the schedule.
+   */
+  delay(fn, amount) {
     const controller = new AbortController();
     if (typeof fn === 'function') {
       const { signal } = controller;
@@ -76,23 +187,19 @@ class MacroCurrentScheduler {
   }
 }
 
-class CurrentScheduler {
-  static get immediate() {
-    return ImmediateCurrentScheduler;
-  }
-
-  static get micro() {
-    return MicroCurrentScheduler;
-  }
-
-  static get macro() {
-    return MacroCurrentScheduler;
-  }
-}
+/* eslint-disable class-methods-use-this */
 
 class Scheduler {
-  static get current() {
-    return CurrentScheduler;
+  get immediate() {
+    return ImmediateScheduler.instance;
+  }
+
+  get micro() {
+    return MicroScheduler.instance;
+  }
+
+  get macro() {
+    return MacroScheduler.instance;
   }
 }
 
