e965d0e81c7828abfe26f9bd60731e97511eeb77
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-08 09:53:39 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -86,10 +86,21 @@ var Scheduler = (function (rxCancellable) {
 
   /* eslint-disable class-methods-use-this */
 
+  /**
+   * @ignore
+   */
   let INSTANCE;
-
+  /**
+   * @ignore
+   */
   const func = x => requestAnimationFrame(x);
+  /**
+   * @ignore
+   */
   const sched = schedule(func);
+  /**
+   * @ignore
+   */
   const timed = delay(func);
   /**
    * A Scheduler that allows immediate scheduling, using requestAnimationFrame.
@@ -131,10 +142,22 @@ var Scheduler = (function (rxCancellable) {
 
   /* eslint-disable class-methods-use-this */
 
+  /**
+   * @ignore
+   */
   let INSTANCE$1;
 
+  /**
+   * @ignore
+   */
   const func$1 = x => Promise.resolve().then(x);
+  /**
+   * @ignore
+   */
   const sched$1 = schedule(func$1);
+  /**
+   * @ignore
+   */
   const timed$1 = delay(func$1);
 
   /**
@@ -178,9 +201,17 @@ var Scheduler = (function (rxCancellable) {
   /* eslint-disable class-methods-use-this */
 
   let INSTANCE$2;
-
+  /**
+   * @ignore
+   */
   const func$2 = x => setTimeout(x, 0);
+  /**
+   * @ignore
+   */
   const sched$2 = schedule(func$2);
+  /**
+   * @ignore
+   */
   const timed$2 = delay(func$2);
   /**
    * A Scheduler that allows timeout scheduling on the current thread.
@@ -222,10 +253,21 @@ var Scheduler = (function (rxCancellable) {
 
   /* eslint-disable class-methods-use-this */
 
+  /**
+   * @ignore
+   */
   let INSTANCE$3;
-
+  /**
+   * @ignore
+   */
   const func$3 = x => x();
+  /**
+   * @ignore
+   */
   const sched$3 = schedule(func$3);
+  /**
+   * @ignore
+   */
   const timed$3 = delay(func$3);
 
   /**
@@ -263,6 +305,236 @@ var Scheduler = (function (rxCancellable) {
      */
     delay(fn, amount) {
       return timed$3(fn, amount);
+    }
+  }
+
+  /* eslint-disable class-methods-use-this */
+
+  /**
+   * @ignore
+   */
+  let INSTANCE$4;
+
+  /**
+   * @ignore
+   */
+  const func$4 = (() => {
+    const script = '(()=>{self.addEventListener("message",a=>self.postMessage(a.data))})();';
+
+    const url = URL.createObjectURL(new Blob([script]));
+
+    const worker = new Worker(url);
+    let busy = false;
+
+    const jobs = [];
+    worker.addEventListener('message', () => {
+      if (jobs.length > 0) {
+        jobs.shift()();
+
+        if (jobs.length > 0) {
+          worker.postMessage(0);
+        } else {
+          busy = false;
+        }
+      }
+    });
+
+    const pushJob = (fn) => {
+      jobs.push(fn);
+
+      if (!busy) {
+        busy = true;
+        worker.postMessage(0);
+      }
+    };
+
+    return pushJob;
+  })();
+  /**
+   * @ignore
+   */
+  const sched$4 = schedule(func$4);
+  /**
+   * @ignore
+   */
+  const timed$4 = delay(func$4);
+
+  /**
+   * A Scheduler that allows scheduling on a separate worker thread.
+   *
+   * Scheduled functions are not executed on the worker thread, rather,
+   * the worker thread emits a message event that schedules the function.
+   */
+  class WorkerScheduler extends SchedulerInterface {
+    static get instance() {
+      if (typeof INSTANCE$4 === 'undefined') {
+        INSTANCE$4 = new WorkerScheduler();
+      }
+      return INSTANCE$4;
+    }
+
+    /**
+     * Schedules the function on the separate worker thread.
+     * @param {!function} fn
+     * A function that is called after being scheduled.
+     * @returns {Cancellable}
+     * Returns an Cancellable that allows
+     * to cancel the schedule.
+     */
+    schedule(fn) {
+      return sched$4(fn);
+    }
+
+    /**
+     * Schedules the function on the separate worker thread.
+     * @param {!function} fn
+     * A function that is called after being scheduled.
+     * @param {!number} amount
+     * The amount of delay in milliseconds.
+     * @returns {Cancellable}
+     * Returns an Cancellable that allows
+     * to cancel the schedule.
+     */
+    delay(fn, amount) {
+      return timed$4(fn, amount);
+    }
+  }
+
+  /* eslint-disable class-methods-use-this */
+
+  /**
+   * @ignore
+   */
+  let INSTANCE$5;
+
+  /**
+   * @ignore
+   */
+  const func$5 = (() => {
+    const script = '(()=>{self.addEventListener("message",a=>self.postMessage(a.data))})();';
+
+    const url = URL.createObjectURL(new Blob([script]));
+
+    const maxWorkers = 256;
+    const totalWorkers = navigator.hardwareConcurrency || 4;
+
+    const workers = [];
+    const jobs = [];
+
+    const recycler = [1];
+    const jobQueue = [];
+
+    const allocate = () => {
+      const i = recycler[0];
+      const n = recycler[i] || 0;
+      if (n === 0) {
+        recycler[0] = i + 1;
+      } else {
+        recycler[0] = n;
+      }
+      recycler[i] = -1;
+      return i;
+    };
+
+    const deallocate = (i) => {
+      if (recycler[i] === -1) {
+        recycler[i] = recycler[0];
+        recycler[0] = i;
+      }
+    };
+
+    const createWorker = () => {
+      const worker = new Worker(url);
+
+      worker.addEventListener('message', ({ data }) => {
+        if (jobs[data]) {
+          jobs[data]();
+          jobs[data] = null;
+          deallocate(data);
+        }
+
+        if (jobQueue.length > 0) {
+          worker.postMessage(jobQueue.shift());
+        } else {
+          workers.push(worker);
+        }
+      });
+
+      return worker;
+    };
+
+    for (let i = 0; i < totalWorkers; i += 1) {
+      workers[i] = createWorker();
+    }
+
+    function pushJob(fn) {
+      const id = allocate();
+      jobs[id] = fn;
+
+      jobQueue.push(id);
+
+      if (workers.length > 0) {
+        workers.shift().postMessage(jobQueue.shift());
+      } else if (totalWorkers < maxWorkers) {
+        createWorker().postMessage(jobQueue.shift());
+      }
+    }
+
+    return pushJob;
+  })();
+  /**
+   * @ignore
+   */
+  const sched$5 = schedule(func$5);
+  /**
+   * @ignore
+   */
+  const timed$5 = delay(func$5);
+
+  /**
+   * A Scheduler that allows scheduling on an idle worker thread from the worker pool.
+   *
+   * Scheduled functions are not executed on the worker thread, rather,
+   * the worker thread emits a message event that schedules the function, in
+   * which the functions are called on the main thread.
+   *
+   * There are a maximum of 256 workers, only 4 or your hardware's concurrency
+   * (nagivator.hardwareConcurrency) size are precreated.
+   *
+   * Execution sequence may be arbitrary due to varying execution times for each worker.
+   */
+  class PoolScheduler extends SchedulerInterface {
+    static get instance() {
+      if (typeof INSTANCE$5 === 'undefined') {
+        INSTANCE$5 = new PoolScheduler();
+      }
+      return INSTANCE$5;
+    }
+
+    /**
+     * Schedules the function on an idle worker thread from the worker pool.
+     * @param {!function} fn
+     * A function that is called after being scheduled.
+     * @returns {Cancellable}
+     * Returns an Cancellable that allows
+     * to cancel the schedule.
+     */
+    schedule(fn) {
+      return sched$5(fn);
+    }
+
+    /**
+     * Schedules the function on an idle worker thread from the worker pool.
+     * @param {!function} fn
+     * A function that is called after being scheduled.
+     * @param {!number} amount
+     * The amount of delay in milliseconds.
+     * @returns {Cancellable}
+     * Returns an Cancellable that allows
+     * to cancel the schedule.
+     */
+    delay(fn, amount) {
+      return timed$5(fn, amount);
     }
   }
 
@@ -315,6 +587,14 @@ var Scheduler = (function (rxCancellable) {
      */
     static get timeout() {
       return TimeoutScheduler.instance;
+    }
+
+    static get worker() {
+      return WorkerScheduler.instance;
+    }
+
+    static get pool() {
+      return PoolScheduler.instance;
     }
   }
 
