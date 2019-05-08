103095e030a04a8eecced7d5678d56d033f23e1b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-05-08 09:53:51 +0800

    Update immediate.browser.js

--- src/immediate.browser.js
+++ src/immediate.browser.js
@@ -2,10 +2,21 @@
 import SchedulerInterface from './scheduler-interface';
 import { schedule, delay } from './utils';
 
+/**
+ * @ignore
+ */
 let INSTANCE;
-
+/**
+ * @ignore
+ */
 const func = x => requestAnimationFrame(x);
+/**
+ * @ignore
+ */
 const sched = schedule(func);
+/**
+ * @ignore
+ */
 const timed = delay(func);
 /**
  * A Scheduler that allows immediate scheduling, using requestAnimationFrame.
